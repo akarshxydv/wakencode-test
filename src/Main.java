9ff6dbac2dcebd9f6bb009df960ca83e84bcbeb5
Akarsh Yadav <akarshxydv@outlook.in>
2024-01-11 16:12:51 +0530

    initial

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
        // find Second Largest
-       int[] arr = {12, 35, 1, 10, 34, 1};
 
-
-       Arrays.sort(arr);
-
-       int max2=arr[arr.length-2];
-        System.out.println(max2);
 
     }
 }
